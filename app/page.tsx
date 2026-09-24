8bf55218944cddf364e680eff8a008f8b95a6520
Dogukanboltul <Dogukanboltul@yandex.com>
2026-09-24 06:01:07 -0700

    Hero silik yazi kaldirildi

--- app/page.tsx
+++ app/page.tsx
@@ -76,7 +76,7 @@ export default function Home() {
 
         <img
           src="/images/toptankadingiyim.jpeg"
-          alt="Kadın giyim tedarik"
+          alt=""
           className="absolute inset-0 h-full w-full object-cover opacity-50"
         />
 
